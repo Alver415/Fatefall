7e17bdf5d6a4130e5812e8f7fdbacc4a601fa0db
Alver415 <Alver415@gmail.com>
2022-04-09 18:06:17 -0400

    Create splash screen on application startup/load

--- modules/fatefall-app-javafx/src/main/java/com/alver/fatefall/app/FatefallApplication.java
+++ modules/fatefall-app-javafx/src/main/java/com/alver/fatefall/app/FatefallApplication.java
@@ -5,11 +5,8 @@ import com.alver.fatefall.app.fx.components.settings.Settings;
 import com.alver.fatefall.app.services.AsyncService;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -31,11 +28,8 @@ public class FatefallApplication extends Application {
     private Settings settings;
 
     @Override
-    public void start(Stage startStage) {
-        startStage.setScene(new Scene(new Label("LOADING")));
-        startStage.setResizable(false);
-        startStage.initStyle(StageStyle.UNDECORATED);
-        startStage.show();
+    public void start(Stage primaryStage) {
+        Stage splash = Splash.createAndShow();
 
         new Thread(() -> {
             //Start Spring Application and get the ApplicationContext
@@ -55,7 +49,7 @@ public class FatefallApplication extends Application {
                 mainStage.getIcons().add(new Image(Objects.requireNonNull(
                         FatefallApplication.class.getResource("icon.png")).toExternalForm()));
                 mainStage.show();
-                startStage.close();
+                splash.close();
             });
         }).start();
     }
