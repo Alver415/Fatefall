a1d82f93a6536f674e9a909f3a23aa1639986111
Alex Alvarez <Alver415@gmail.com>
2023-07-20 02:57:36 -0400

    wip broken

--- fatefall-service/src/main/java/com/alver/fatefall/data/entities/PersistedEntity.java
+++ fatefall-service/src/main/java/com/alver/fatefall/data/entities/PersistedEntity.java
@@ -1,5 +1,9 @@
 package com.alver.fatefall.data.entities;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -8,20 +12,23 @@ import org.springframework.data.domain.Persistable;
 import java.util.ArrayList;
 import java.util.List;
 
-@MappedSuperclass
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class PersistedEntity {
 
 	@Id
 	@GeneratedValue
+	@JsonIgnore
 	private Long id;
 
 	public Long getId() {
 		return id;
 	}
-	
+
 	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
 	@JoinColumn(name = "id")
 	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
 	private List<Field> fields = new ArrayList<>();
 
 	public List<Field> getFields() {
